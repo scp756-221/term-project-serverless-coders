83e02709121f900a35864414786a49d239f1d611
UniVerge <hza164@sfu.ca>
2022-03-20 00:41:29 -0700

    Gatling (#34)
    * add files
    
    * add load playlist
    
    * add playlist.csv
    
    * fix typo
    
    * initialize gatling
    
    * change readme
    
    * initialize gatling
    
    * initialize gatling
    
    * add some notes
    
    Co-authored-by: jmc0 <fcnclk@outlook.com>

--- gatling/simulations/proj756/ReadTables.scala
+++ gatling/simulations/proj756/ReadTables.scala
@@ -5,6 +5,13 @@ import scala.concurrent.duration._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
+/*
+TODO:
+1. You must run your application with up to a load of thousands of users (e.g, >1000 users) and hundreds of thousands requests (e.g., >100,00 requests/second).
+2. Other tests besides just reading from the table
+*/
+
+
 object Utility {
   /*
     Utility to get an Int from an environment variable.
@@ -28,6 +35,9 @@ object Utility {
   }
 }
 
+/*
+  Read music
+*/
 object RMusic {
 
   val feeder = csv("music.csv").eager.random
@@ -41,6 +51,9 @@ object RMusic {
 
 }
 
+/*
+  Read user
+*/
 object RUser {
 
   val feeder = csv("users.csv").eager.circular
@@ -49,6 +62,22 @@ object RUser {
     feed(feeder)
     .exec(http("RUser ${i}")
       .get("/api/v1/user/${UUID}"))
+    .pause(1)
+  }
+
+}
+
+/*
+  Read playlist
+*/
+object RPlaylist {
+
+  val feeder = csv("playlist.csv").eager.circular
+
+  val rplaylist = forever("i") {
+    feed(feeder)
+    .exec(http("RPlaylist ${i}")
+      .get("/api/v1/playlist/${UUID}"))
     .pause(1)
   }
 
@@ -84,7 +113,22 @@ object RMusicVarying {
 }
 
 /*
-  Failed attempt to interleave reads from User and Music tables.
+  After one S3 read, pause a random time between 1 and 60 s
+*/
+
+object RPlaylistVarying {
+  val feeder = csv("playlist.csv").eager.circular
+
+  val rmusic = forever("i") {
+    feed(feeder)
+    .exec(http("RPlaylistVarying ${i}")
+      .get("/api/v1/playlist/${UUID}"))
+    .pause(1, 60)
+  }
+}
+
+/*
+  Failed attempt to interleave reads from User, Music, Playlist tables.
   The Gatling EDSL only honours the second (Music) read,
   ignoring the first read of User. [Shrug-emoji] 
  */
@@ -92,6 +136,7 @@ object RBoth {
 
   val u_feeder = csv("users.csv").eager.circular
   val m_feeder = csv("music.csv").eager.random
+  val p_feeder = csv("playlist.csv").eager.random
 
   val rboth = forever("i") {
     feed(u_feeder)
@@ -102,7 +147,12 @@ object RBoth {
     feed(m_feeder)
     .exec(http("RMusic ${i}")
       .get("/api/v1/music/${UUID}"))
-      .pause(1)
+    .pause(1);
+
+    feed(p_feeder)
+    .exec(http("RPlaylist ${i}")
+      .get("/api/v1/playlist/${UUID}"))
+    .pause(1);
   }
 
 }
@@ -134,14 +184,26 @@ class ReadMusicSim extends ReadTablesSim {
   ).protocols(httpProtocol)
 }
 
+class ReadPlaylistSim extends ReadTablesSim {
+  val scnReadPlaylist = scenario("ReadPlaylist")
+    .exec(RPlaylist.rplaylist)
+
+  setUp(
+    scnReadPlaylist.inject(atOnceUsers(Utility.envVarToInt("USERS", 1)))
+  ).protocols(httpProtocol)
+}
+
 /*
-  Read both services concurrently at varying rates.
+  Read all services concurrently at varying rates.
   Ramp up new users one / 10 s until requested USERS
   is reached for each service.
 */
-class ReadBothVaryingSim extends ReadTablesSim {
+class ReadAllVaryingSim extends ReadTablesSim {
   val scnReadMV = scenario("ReadMusicVarying")
     .exec(RMusicVarying.rmusic)
+  
+  val scnReadPV = scenario("ReadPlaylistVarying")
+    .exec(RPlaylistVarying.rplaylist)
 
   val scnReadUV = scenario("ReadUserVarying")
     .exec(RUserVarying.ruser)
@@ -151,6 +213,7 @@ class ReadBothVaryingSim extends ReadTablesSim {
   setUp(
     // Add one user per 10 s up to specified value
     scnReadMV.inject(rampConcurrentUsers(1).to(users).during(10*users)),
+    scnReadPV.inject(rampConcurrentUsers(1).to(users).during(10*users)),
     scnReadUV.inject(rampConcurrentUsers(1).to(users).during(10*users))
   ).protocols(httpProtocol)
 }
@@ -161,12 +224,12 @@ class ReadBothVaryingSim extends ReadTablesSim {
   (or a warning that this approach will fail).
  */
 /*
-class ReadBothSim extends ReadTablesSim {
-  val scnReadBoth = scenario("ReadBoth")
-    .exec(RBoth.rboth)
+class ReadAllSim extends ReadTablesSim {
+  val scnReadAll = scenario("ReadAll")
+    .exec(RAll.rall)
 
   setUp(
-    scnReadBoth.inject(atOnceUsers(1))
+    scnReadAll.inject(atOnceUsers(1))
   ).protocols(httpProtocol)
 }
 */
