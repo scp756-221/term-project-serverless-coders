592b2148d4dbd6af3d4a726620daaffb9226a0ed
jmc0 <fcnclk@outlook.com>
2022-04-04 19:06:51 -0700

    fix ci

--- s3/app.py
+++ s3/app.py
@@ -24,7 +24,7 @@ import simplejson as json
 
 # Integer value 0 <= v < 100, denoting proportion of
 # calls to `get_song` to return 500 from
-PERCENT_ERROR = 50
+PERCENT_ERROR = 0
 
 app = Flask(__name__)
 
@@ -66,10 +66,10 @@ def get_playlist(playlist_id):
     payload = {"objtype": "playlist", "objkey": playlist_id}
 
     # This version will return 500 for a fraction of its calls
-    # if random.randrange(100) < PERCENT_ERROR:
-    #     return Response(json.dumps({"error": "get_playlist failed"}),
-    #                     status=500,
-    #                     mimetype='application/json')
+    if random.randrange(100) < PERCENT_ERROR:
+        return Response(json.dumps({"error": "get_playlist failed"}),
+                        status=500,
+                        mimetype='application/json')
 
     url = db['name'] + '/' + db['endpoint'][0]
     response = requests.get(
